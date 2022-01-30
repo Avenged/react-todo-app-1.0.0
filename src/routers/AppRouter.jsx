ccc44b24cb47255e4ddceea4c804481e2152c034
José Chaudary <64991405+Avenged@users.noreply.github.com>
2022-01-30 00:08:30 -0300

    Volver a hashrouter

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { 
-    BrowserRouter as Router,
+    HashRouter as Router,
     Redirect,
     Switch,
 } from "react-router-dom";
